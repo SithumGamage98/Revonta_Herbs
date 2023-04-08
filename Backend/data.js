f2b66d700c507b7b81b80049cffc1457b342447e
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-04-08 08:17:21 +0530

    added backend files up to MongoDB connection

---
+++ Backend/data.js
@@ -0,0 +1,62 @@
+//import galle from '../images/Travelo.jpeg'
+
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'Aloe Vera',
+      slug: 'Alovera-Organic-Mix',
+      category: 'Cream',
+      image: '../images/Aloe_Vera.jpg',
+      price: 1200,
+      countInStock: 10,
+      brand: 'Aloe vera',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality faceial product',
+    },
+
+    {
+      _id: '2',
+      name: 'Skin Green Tea',
+      slug: 'Skin-Green-Tea',
+      category: 'Green Tea',
+      image: '../images/Skin_care.jpg',
+      price: 12,
+      countInStock: 0,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+
+    {
+      _id: '3',
+      name: 'Brand Green Tea',
+      slug: 'Brand-green-tea',
+      category: 'GreenTea',
+      image: '../images/Brand_green_tea.jpg',
+      price: 12,
+      countInStock: 15,
+      brand: 'Brand',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+
+    {
+      _id: '4',
+      name: 'Ayur Green Tea',
+      slug: 'Ayur-green-tea',
+      category: 'GreenTea',
+      image: '../images/GreenTea.jpg',
+      price: 12,
+      countInStock: 15,
+      brand: 'Ayur',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
---
+++ Backend/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+  res.send(data.products);
+});
+///api/products/slug/
+app.get('/api/products/slug/:slug', (req, res) => {
+  const product = data.products.find((x) => x.slug == req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'product not found' });
+  }
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id == req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'product not found' });
+  }
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`server at http://localhost:${port}`);
+});
