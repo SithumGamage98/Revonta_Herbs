1b364d4ea021a674a7ee1694102eaa88e09f70b7
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-04-12 21:34:02 +0530

    added updated Signingn Screen and Shipping Address Screen

---
+++ src/screens/ShippingAddressScreen.js
@@ -0,0 +1,125 @@
+import React, { useContext, useEffect } from 'react';
+import { Helmet } from 'react-helmet-async';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+import { Store } from '../Store';
+import { useState } from 'react';
+//import CheckoutSteps from '../components/CheckoutSteps';
+
+export default function ShippingAddressScreen() {
+  const navigate = useNavigate();
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+
+  const {
+    userInfo,
+    cart: { shippingAddress },
+  } = state;
+  const [fullName, setFullName] = useState(shippingAddress.fullName || '');
+  const [address, setAddress] = useState(shippingAddress.address || '');
+  const [city, setCity] = useState(shippingAddress.city || '');
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress.postalCode || ''
+  );
+  const [country, setCountry] = useState(shippingAddress.country || '');
+
+  //store data permenently
+  useEffect(() => {
+    if (!userInfo) {
+      navigate('/signin?redirect=/shipping');
+    }
+  }, [userInfo, navigate]);
+
+  //submitHandler function
+  const submitHandler = (e) => {
+    e.preventDefault();
+    ctxDispatch({
+      type: 'SAVE_SHIPPING_ADDRESS',
+      payload: {
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+      },
+    });
+    localStorage.setItem(
+      'shippingAddress',
+      JSON.stringify({
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+      })
+    );
+    navigate('/payment');
+  };
+
+  return (
+    <div>
+      <Helmet>
+        <title>Shipping Address</title>
+      </Helmet>
+
+      <div className="container small-container">
+        <h1 className="my-3">Shipping Address</h1>
+
+        <Form onSubmit={submitHandler}>
+          {/*Full Name */}
+          <Form.Group className="mb-3" controlId="fullName">
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              required
+            />
+          </Form.Group>
+          {/*Address */}
+          <Form.Group className="mb-3" controlId="address">
+            <Form.Label>Home Address</Form.Label>
+            <Form.Control
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              required
+            />
+          </Form.Group>
+          {/*City */}
+          <Form.Group className="mb-3" controlId="city">
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              required
+            />
+          </Form.Group>
+
+          {/*postalCode */}
+          <Form.Group className="mb-3" controlId="postalCode">
+            <Form.Label>postal Code</Form.Label>
+            <Form.Control
+              value={postalCode}
+              onChange={(e) => setPostalCode(e.target.value)}
+              required
+            />
+          </Form.Group>
+
+          {/*Country */}
+          <Form.Group className="mb-3" controlId="country">
+            <Form.Label>Country</Form.Label>
+            <Form.Control
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <div className="mb-3">
+            <Button variant="primary" type="submit">
+              Continue
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+}
