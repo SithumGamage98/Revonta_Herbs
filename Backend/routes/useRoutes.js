e9b72552c52a5d2a7a4415374287070b38e397b8
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-04-14 22:25:38 +0530

    Finished user profile API and update user details

--- Backend/routes/useRoutes.js
+++ Backend/routes/useRoutes.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs';
 import expressAsyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 import { generateToken } from '../utils.js';
+import { isAuth } from '../utils.js';
 
 const userRouter = express.Router();
 
@@ -48,6 +49,33 @@ userRouter.post(
       isAdmin: user.isAdmin,
       token: generateToken(user), //Create a tocken for the each of the user
     });
+  })
+);
+
+//For Update User Profile Data
+userRouter.put(
+  '/profile',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8);
+      }
+
+      const updatedUser = await user.save();
+      res.send({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser),
+      });
+    } else {
+      res.status(404).send({ message: 'User not found..' });
+    }
   })
 );
 
