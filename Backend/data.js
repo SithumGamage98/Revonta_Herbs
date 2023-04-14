bb5aa631dbac47f8f60cdf4f5f9d8b5fb4a3c11c
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-04-15 01:12:02 +0530

    added few important missing files

--- Backend/data.js
+++ Backend/data.js
@@ -1,25 +1,45 @@
 //import galle from '../images/Travelo.jpeg'
-
+import bcrypt from 'bcryptjs';
 const data = {
+  //user's data
+
+  users: [
+    {
+      //Admin user
+      name: 'Sithum',
+      email: 'sithum98@gmail.com',
+      password: bcrypt.hashSync('1234'),
+      isAdmin: true,
+    },
+    {
+      //normal user
+      name: 'Heshan',
+      email: 'Heshan98@gmail.com',
+      password: bcrypt.hashSync('9999'),
+      isAdmin: false,
+    },
+  ],
+
+  //Products data
   products: [
     {
-      _id: '1',
-      name: 'Aloe Vera',
+      // _id: '1',
+      name: 'Aloe Vera index',
       slug: 'Alovera-Organic-Mix',
       category: 'Cream',
       image: '../images/Aloe_Vera.jpg',
       price: 1200,
       countInStock: 10,
-      brand: 'Aloe vera',
+      brand: 'Aloe vera pvt',
       rating: 4.5,
       numReviews: 10,
       description: 'high quality faceial product',
     },
 
     {
-      _id: '2',
+      //_id: '2',
       name: 'Skin Green Tea',
-      slug: 'Skin-Green-Tea',
+      slug: 'Skin-Green-Tea2',
       category: 'Green Tea',
       image: '../images/Skin_care.jpg',
       price: 12,
@@ -27,35 +47,35 @@ const data = {
       brand: 'Nike',
       rating: 4.5,
       numReviews: 14,
-      description: 'high quality product',
+      description: 'high quality product1',
     },
 
     {
-      _id: '3',
+      // _id: '3',
       name: 'Brand Green Tea',
       slug: 'Brand-green-tea',
       category: 'GreenTea',
       image: '../images/Brand_green_tea.jpg',
       price: 12,
       countInStock: 15,
-      brand: 'Brand',
+      brand: 'Brand yyt',
       rating: 4.5,
       numReviews: 14,
-      description: 'high quality product',
+      description: 'high quality product11',
     },
 
     {
-      _id: '4',
+      // _id: '4',
       name: 'Ayur Green Tea',
       slug: 'Ayur-green-tea',
-      category: 'GreenTea',
+      category: 'GreenTea778',
       image: '../images/GreenTea.jpg',
       price: 12,
       countInStock: 15,
-      brand: 'Ayur',
+      brand: 'Ayur pvt',
       rating: 4.5,
       numReviews: 14,
-      description: 'high quality product',
+      description: 'high quality product7766',
     },
   ],
 };
