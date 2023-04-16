98c67ab24c4cb9634ebeca5d674611bdb3b1ab4d
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-04-16 09:32:58 +0530

    Added updated few  backend Files

---
+++ Backend/Order_and_Payment_Management_Service/Routes/Order_Route.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import {
+  creat_Order,
+  get_orders,
+  get_orders_byId,
+  update_order,
+} from '../Controllers/Order_Controller';
+
+const orderRouter = express.Router();
+
+orderRouter.post('/', creat_Order);
+orderRouter.get('/mine', get_orders);
+orderRouter.get('/:id', get_orders_byId);
+orderRouter.put('/:id/pay', update_order);
+
+export default orderRouter;
---
+++ Backend/Product_Management_Service/Controllers/Product_Controller.js
@@ -0,0 +1,118 @@
+import express from 'express';
+import Product from '../../models/productModel';
+
+//get all products
+const get_products = async function (req, res) {
+  const products = await Product.find();
+  res.send(products); //from product model
+};
+
+//Search/Filter Products
+const PAGE_SIZE = 3; //default page size
+const search_products = async function (req, res) {
+  const { query } = req;
+  const pageSize = query.pageSize || PAGE_SIZE;
+  const page = query.page || 1;
+  const category = query.category || '';
+  const price = query.price || '';
+  const rating = query.rating || '';
+  const order = query.order || '';
+  const searchQuery = query.query || '';
+
+  const queryFilter =
+    searchQuery && searchQuery !== 'all'
+      ? {
+          name: {
+            $regex: searchQuery,
+            $options: 'i',
+          },
+        }
+      : {};
+  const categoryFilter = category && category !== 'all' ? { category } : {};
+  const ratingFilter =
+    rating && rating !== 'all'
+      ? {
+          rating: {
+            $gte: Number(rating),
+          },
+        }
+      : {};
+  const priceFilter =
+    price && price !== 'all'
+      ? {
+          // 1-50
+          price: {
+            $gte: Number(price.split('-')[0]),
+            $lte: Number(price.split('-')[1]),
+          },
+        }
+      : {};
+  const sortOrder =
+    order === 'featured'
+      ? { featured: -1 }
+      : order === 'lowest'
+      ? { price: 1 }
+      : order === 'highest'
+      ? { price: -1 }
+      : order === 'toprated'
+      ? { rating: -1 }
+      : order === 'newest'
+      ? { createdAt: -1 }
+      : { _id: -1 };
+
+  const products = await Product.find({
+    ...queryFilter,
+    ...categoryFilter,
+    ...priceFilter,
+    ...ratingFilter,
+  })
+    .sort(sortOrder)
+    .skip(pageSize * (page - 1))
+    .limit(pageSize);
+
+  const countProducts = await Product.countDocuments({
+    ...queryFilter,
+    ...categoryFilter,
+    ...priceFilter,
+    ...ratingFilter,
+  });
+  res.send({
+    products,
+    countProducts,
+    page,
+    pages: Math.ceil(countProducts / pageSize),
+  });
+};
+
+//For Side bar functions -> Retriev Categories
+const get_ProductCategories = async function (req, res) {
+  const categories = await Product.find().distinct('category');
+  res.send(categories);
+};
+
+//get product slugs
+const get_ProductSlugs = async function (req, res) {
+  const product = await Product.findOne({ slug: req.params.slug });
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+};
+
+const get_productsById = async function (req, res) {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+};
+
+export default {
+  search_products,
+  get_products,
+  get_ProductSlugs,
+  get_productsById,
+  get_ProductCategories,
+};
