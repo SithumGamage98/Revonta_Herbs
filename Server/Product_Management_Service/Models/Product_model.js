36aa3f71257addf5cb41127c1c565bc586eb2ac3
IT20617332 <siridana@gmail.com>
2023-04-17 18:22:46 +0530

    product routes rearranged

--- Server/Product_Management_Service/Models/Product_model.js
+++ Server/Product_Management_Service/Models/Product_model.js
@@ -2,16 +2,16 @@ import mongoose from 'mongoose';
 
 const productSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    slug: { type: String, required: true },
-    category: { type: String, required: true },
-    image: { type: String, required: true },
-    price: { type: Number, required: true },
+    name: {   type: String, required: true },
+    slug: {   type: String, required: true },
+    category: {   type: String, required: true },
+    image: {  type: String, required: true },
+    price: {  type: Number, required: true },
     countInStock: { type: Number, required: true },
-    brand: { type: String, required: true },
-    rating: { type: Number, required: true },
-    numReviews: { type: Number, required: true },
-    description: { type: String, required: true },
+    brand: {  type: String, required: true },
+    rating: {   type: Number, required: true },
+    numReviews: {   type: Number, required: true },
+    description: {  type: String, required: true },
   },
   {
     timestamps: true,
