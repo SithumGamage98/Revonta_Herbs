dac0f7348d1a56d0b38d7ac34c7fde03cf0db444
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-04-17 08:26:56 +0530

    Finished utill.js file with proper validation and authentications

---
+++ Server/utils.js
@@ -0,0 +1,38 @@
+//impolement JSON Web Tocken/JWT
+
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (user) => {
+  //calll sign function
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET, // 2nd para
+    {
+      expiresIn: '30d',
+    }
+  );
+};
+
+//Implement isAuth Middleware function
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      //callBack function
+      if (err) {
+        res.status(401).send({ message: 'Invalid Token' });
+      } else {
+        req.user = decode;
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({ message: 'No Token' });
+  }
+};
