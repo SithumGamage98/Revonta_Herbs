06e8dc97aa2e08bb4ffaa4bc27d47d044863ff3f
IT20617332 <siridana@gmail.com>
2023-04-18 22:18:33 +0530

    user controller attributes modified

--- Server/User_Management_Service/Controllers/User_Controller.js
+++ Server/User_Management_Service/Controllers/User_Controller.js
@@ -12,11 +12,11 @@ const user_signIn = async function (req, res) {
     if (bcrypt.compareSync(req.body.password, user.password)) {
       //1 st para -> Visible/printed Password / 2nd para -> increpted password (bcrypt is use for create an encript password)
       res.send({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user), //Create a tocken for the each of the user
+         _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user), //Create a tocken for the each of the user
       });
       return;
     }
@@ -27,18 +27,18 @@ const user_signIn = async function (req, res) {
 // For Sign Up
 const user_signUp = async function (req, res) {
   const newUser = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password),
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
   });
   // Save new user in the database
   const user = await newUser.save();
   res.send({
-    _id: user._id,
-    name: user.name,
-    email: user.email,
-    isAdmin: user.isAdmin,
-    token: generateToken(user), //Create a tocken for the each of the user
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user), //Create a tocken for the each of the user
   });
 };
 
@@ -54,11 +54,11 @@ const user_update = async function (req, res) {
 
     const updatedUser = await user.save();
     res.send({
-      _id: updatedUser._id,
-      name: updatedUser.name,
-      email: updatedUser.email,
-      isAdmin: updatedUser.isAdmin,
-      token: generateToken(updatedUser),
+        _id:  updatedUser._id,
+        name:   updatedUser.name,
+        email:  updatedUser.email,
+        isAdmin:  updatedUser.isAdmin,
+        token:  generateToken(updatedUser),
     });
   } else {
     res.status(404).send({ message: 'User not found..' });
