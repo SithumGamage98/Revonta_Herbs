cb479d01cc150704958582eddc5643862a91024c
IT20617332 <siridana@gmail.com>
2023-04-18 22:17:24 +0530

    Rating file altered with values

--- Client/src/components/Rating.js
+++ Client/src/components/Rating.js
@@ -6,10 +6,10 @@ function Rating(props) {
         <i
           className={
             rating >= 1
-              ? 'fas fa-star'
-              : rating >= 0.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+                ? 'fas fa-star'
+                : rating >= 0.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
           }
         />
       </span>
@@ -17,10 +17,10 @@ function Rating(props) {
         <i
           className={
             rating >= 2
-              ? 'fas fa-star'
-              : rating >= 1.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+                ? 'fas fa-star'
+                : rating >= 1.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
           }
         />
       </span>
@@ -28,10 +28,10 @@ function Rating(props) {
         <i
           className={
             rating >= 3
-              ? 'fas fa-star'
-              : rating >= 2.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+                ? 'fas fa-star'
+                : rating >= 2.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
           }
         />
       </span>
@@ -39,10 +39,10 @@ function Rating(props) {
         <i
           className={
             rating >= 4
-              ? 'fas fa-star'
-              : rating >= 3.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+                ? 'fas fa-star'
+                : rating >= 3.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
           }
         />
       </span>
@@ -50,10 +50,10 @@ function Rating(props) {
         <i
           className={
             rating >= 5
-              ? 'fas fa-star'
-              : rating >= 4.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
+                ? 'fas fa-star'
+                : rating >= 4.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
           }
         />
       </span>
