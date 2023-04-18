8c1e79df0a20ee9dbc8dd76ec47004a1e39144f1
IT20617332 <siridana@gmail.com>
2023-04-18 22:15:56 +0530

    searchbox modified

--- Client/src/components/Product.js
+++ Client/src/components/Product.js
@@ -43,7 +43,7 @@ function Product(props) {
         <Card.Text>${product.price}</Card.Text>
         {product.countInStock === 0 ? (
           <Button variant="light" disabled>
-            {/* Out of stock */}
+            Out of stock
           </Button>
         ) : (
           <Button onClick={() => addToCartHandler(product)}>Add to cart</Button>
