6ee0af6480e286827a338c9a6804752991492a53
IT20617332 <siridana@gmail.com>
2023-04-19 09:59:08 +0530

    new backend file added

--- Client/src/components/SearchBox.js
+++ Client/src/components/SearchBox.js
@@ -9,20 +9,20 @@ export default function SearchBox() {
     const navigate = useNavigate();
     const [query, setQuery] = useState('');
     const submitHandler = (e) => {
-      e.preventDefault();
-      navigate(query ? `/search/?query=${query}` : '/search');
+        e.preventDefault();
+        navigate(query ? `/search/?query=${query}` : '/search');
   };
   return (
     <Form className="d-flex me-auto" onSubmit={submitHandler}>
       <InputGroup>
         <FormControl
-              type="text"
-              name="q"
-              id="q"
-              onChange={(e) => setQuery(e.target.value)}
-              placeholder="search products Here..."
-              aria-label="Search Products"
-              aria-describedby="button-search"
+                type="text"
+                name="q"
+                id="q"
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="search products Here..."
+                aria-label="Search Products"
+                aria-describedby="button-search"
         ></FormControl>
         <Button variant="outline-primary" type="submit" id="button-search">
           <i className="fas fa-search"></i>
