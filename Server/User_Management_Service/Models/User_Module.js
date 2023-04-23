bb9b97cb749f11a168b0d49785b94033fd48b4f6
Lushani Odara Amarakoon <86650379+OdaraAmarakoon@users.noreply.github.com>
2023-04-23 13:34:05 +0530

    Update User_Module.js

--- Server/User_Management_Service/Models/User_Module.js
+++ Server/User_Management_Service/Models/User_Module.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema(
       password: { type: String, required: true },
       isAdmin: { type: Boolean, default: false, required: true },
   },
+  
   {
       timestamps: true,
   }
