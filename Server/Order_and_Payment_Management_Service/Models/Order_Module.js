be97a3f316c46cf30d68135a01d53fd79501aed8
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-04-25 09:09:02 +0530

    Did some changes in delivery model

--- Server/Order_and_Payment_Management_Service/Models/Order_Module.js
+++ Server/Order_and_Payment_Management_Service/Models/Order_Module.js
@@ -16,13 +16,7 @@ const orderSchema = new mongoose.Schema(
         },
       },
     ],
-    shippingAddress: {
-      fullName: { type: String, required: true },
-      address: { type: String, required: true },
-      city: { type: String, required: true },
-      postalCode: { type: String, required: true },
-      country: { type: String, required: true },
-    },
+
     paymentMethod: { type: String, required: true },
     paymentResult: {
       id: String,
