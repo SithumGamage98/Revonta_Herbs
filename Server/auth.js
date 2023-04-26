09dc9861406679c025dd2dbe4e2ca934655a5fc9
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-04-26 22:24:26 +0530

    Did some changes in folder structure

--- Server/auth.js
+++ Server/auth.js
@@ -1,9 +1,10 @@
-// impolement JSON Web Tocken/JWT
+//impolement JSON Web Tocken/JWT
 
 import jwt from 'jsonwebtoken';
+import mg from 'mailgun-js';
 
 export const generateToken = (user) => {
-  // calll sign function
+  //calll sign function
   return jwt.sign(
     {
       _id: user._id,
@@ -18,14 +19,12 @@ export const generateToken = (user) => {
   );
 };
 
-// Implement isAuth Middleware function
+//Implement isAuth Middleware function
 export const isAuth = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (authorization) {
-    const token = authorization.slice(7, authorization.length);
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
     jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
-      // callBack function
-
       if (err) {
         res.status(401).send({ message: 'Invalid Token' });
       } else {
@@ -36,4 +35,80 @@ export const isAuth = (req, res, next) => {
   } else {
     res.status(401).send({ message: 'No Token' });
   }
+};
+
+//For admin
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: 'Invalid Admin Token' });
+  }
+};
+
+//Send Emails -> Mailgun Authentication
+export const mailgun = () =>
+  mg({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMIAN,
+  });
+
+//Email body
+export const payOrderEmailTemplate = (order) => {
+  return `<h1>Thanks for shopping with Revonta Herbs(PVT)</h1>
+  <p>
+  Hi ${order.user.name},</p>
+  <p>We have finished processing your order.</p>
+  <h2>[Order ${order._id}] (${order.createdAt.toString().substring(0, 10)})</h2>
+  <table>
+  <thead>
+  <tr>
+  <td><strong>Product</strong></td>
+  <td><strong>Quantity</strong></td>
+  <td><strong align="right">Price</strong></td>
+  </thead>
+  <tbody>
+  ${order.orderItems
+    .map(
+      (item) => `
+    <tr>
+    <td>${item.name}</td>
+    <td align="center">${item.quantity}</td>
+    <td align="right"> $${item.price.toFixed(2)}</td>
+    </tr>
+  `
+    )
+    .join('\n')}
+  </tbody>
+  <tfoot>
+  <tr>
+  <td colspan="2">Items Price:</td>
+  <td align="right"> $${order.itemsPrice.toFixed(2)}</td>
+  </tr>
+  <tr>
+  <td colspan="2">Shipping Price:</td>
+  <td align="right"> $${order.shippingPrice.toFixed(2)}</td>
+  </tr>
+  <tr>
+  <td colspan="2"><strong>Total Price:</strong></td>
+  <td align="right"><strong> $${order.totalPrice.toFixed(2)}</strong></td>
+  </tr>
+  <tr>
+  <td colspan="2">Payment Method:</td>
+  <td align="right">${order.paymentMethod}</td>
+  </tr>
+  </table>
+  <h2>Shipping address</h2>
+  <p>
+  ${order.shippingAddress.fullName},<br/>
+  ${order.shippingAddress.address},<br/>
+  ${order.shippingAddress.city},<br/>
+  ${order.shippingAddress.country},<br/>
+  ${order.shippingAddress.postalCode}<br/>
+  </p>
+  <hr/>
+  <p>
+  Thanks for shopping with us.!
+  </p>
+  `;
 };
