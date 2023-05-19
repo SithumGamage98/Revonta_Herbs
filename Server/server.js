c14e325350a25348510f7ec989c39144664dfabc
IT20617332 <siridana@gmail.com>
2023-05-19 18:54:50 +0530

    add all

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import data from './data.js';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import seedRouter from './routes/seedRoutes.js';
-import productRouter from './routes/productRouter.js';
-import userRouter from './routes/useRoutes.js';
-import orderRouter from './routes/orderRoutes.js';
-import uploadRouter from './routes/uploadRoutes.js';
+import express from "express";
+import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRouter.js";
+import userRouter from "./routes/useRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import uploadRouter from "./routes/uploadRoutes.js";
 
 dotenv.config();
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('connected to db');
+    console.log("connected to db");
   })
   .catch((err) => {
     console.log(err.message);
@@ -23,32 +23,32 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //Upload file
-app.use('/api/upload', uploadRouter);
+app.use("/api/upload", uploadRouter);
 //Paypal
-app.get('/api/keys/paypal', (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+app.get("/api/keys/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
 //for routing Seed
-app.use('/api/seed', seedRouter);
+app.use("/api/seed", seedRouter);
 
 //Get all products router
-app.use('/api/products', productRouter);
+app.use("/api/products", productRouter);
 
 //userRouter
-app.use('/api/users', userRouter);
+app.use("/api/users", userRouter);
 
 //Error handler for Express
 //app.use('/api/users', userRouter);
 
 //OrderRouter
-app.use('/api/orders', orderRouter);
+app.use("/api/orders", orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8081;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
 });
